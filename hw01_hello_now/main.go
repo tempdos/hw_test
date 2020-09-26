0aa3354395ce2bd204fa5e4e0f8a83ead55e71ff
tempdos <tempdos@gmail.com>
2020-09-27 01:10:21 +0900

    hw01_completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-  print("test")
+	timer, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("current time:", time.Now())
+	fmt.Println("exact time:", timer)
 }
